feb48db8f16bfaa7f8c5409a4c5ac22ce1bfe09a
Rivath <163083945+SinPhourivath@users.noreply.github.com>
2025-12-11 12:02:20 +0700

    configure strict type checked and add Tailwindcss

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,8 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
